669dac4dae07ea4f78a8bffbb32d125eb633c77f
Aidan Robinson <aidanrobinson6@gmail.com>
2019-08-26 11:49:31 +1200

    Brick_BreakerV1
    Starting on the UI of the game

---
+++ New Unity Project/Assets/_Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    public float ballInitialVelocity = 600f;
+
+    private Rigidbody rb;
+    private bool ballInPlay;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Jump") && ballInPlay == false)
+        {
+            transform.parent = null;
+            ballInPlay = true;
+            rb.isKinematic = false;
+            rb.AddForce(new Vector3(ballInitialVelocity, ballInitialVelocity, 0));
+        }
+    }
+}
